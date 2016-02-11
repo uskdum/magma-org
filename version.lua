5ae58648c1e01b03ef7b90b9aabf89c2f1afc07b
uskdum <8uskdum@30mail.ir>
2016-02-11 12:31:38 +0330

    Create version.lua

---
+++ version.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+ retrun 'magma bot test v.1'..[[
+   
+   Powered by: amir w.h 
+   maker plugin : amir w.h
+   admin server: amir w.h
+   sudo: amir w.h]]
+   
+end
+
+return {
+   description = "about bot",
+   usage = "ver : about bot",
+   patterns = {"ver"
+   
+   },
+   run=run
+ }
+ end
+   
