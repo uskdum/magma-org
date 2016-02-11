2152e35be4f2c604852251fd2441858253e4398d
uskdum <8uskdum@30mail.ir>
2016-02-11 15:05:02 +0330

    Create sudo.lua

---
+++ sudo.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return [[
+ sudo of telemagma : 
+amir w.h
+amir pga
+  ]]
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^SUDOERS$",
+    "^/sudoers$",
+    "^!sudoers$",
+    "^sudoers$",
+  },
+  run = run
+}
+end
